cff83b003880beabfcb2fe6b8b5ec920586c702c
Ahmed Al-Sabounchi <a_m_96@live.com>
2023-08-28 18:39:42 -0400

    Created echo server that handles one connection

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ahm1388/realtime-chat
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/ahm1388/realtime-chat/config"
+	"github.com/ahm1388/realtime-chat/server"
+)
+ 
+func setupFlags () {
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "host for the server")
+	flag.IntVar(&config.Port, "port", 7007, "port for the server")
+}
+func main () {
+	setupFlags()
+	log.Println("Listening for statement to echo...")
+	server.RunSyncTCPServer()
+}
